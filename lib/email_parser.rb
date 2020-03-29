2940800515567a451b5260f8ba0090b9c01614ad
Billy Ferrara <willfira717@gmail.com>
2020-03-29 17:58:00 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,9 +13,9 @@ class EmailAddressParser
 
 
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+    @emails = @emails.gsub(/[\s,]/ ," ").split
+  		# @email is an array now
+  		@emails.uniq
   end
 
 end
